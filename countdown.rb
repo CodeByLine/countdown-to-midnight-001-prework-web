d535cc8308dc35ac91840cd464edcf1f3df09841
CodeByLine <yleventhal@ymail.com>
2016-01-19 19:20:58 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
 #write your code here
 
 def countdown
+  my_counter = 10
+  while my_counter > 0
+    puts "#{my_counter} SECOND(S)!"
+    my_counter -= 1
+  end 
+    
+   puts "HAPPY NEW YEAR!"
 end
