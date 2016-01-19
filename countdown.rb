e6ae690aff014d2c35f4abd9f9505b760a1af14e
CodeByLine <yleventhal@ymail.com>
2016-01-19 19:33:25 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,25 @@
 #write your code here
 
-def countdown
-  my_counter = 10
-  while my_counter > 0
-    puts "#{my_counter} SECOND(S)!"
-    my_counter -= 1
-  end 
-    
-   puts "HAPPY NEW YEAR!"
+def countdown(my_counter)
+
+   my_counter = 10
+   until my_counter == 0
+     puts "#{my_counter} SECOND(S)!"
+     my_counter -= 1
+   end
+
+  return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(my_counter)
+     my_counter = 10
+     until my_counter == 0
+     puts "#{my_counter} SECOND(S)!"
+     my_counter -= 1
+     sleep(1)
+   end
+
+  return "HAPPY NEW YEAR!"
+end
+
+  
